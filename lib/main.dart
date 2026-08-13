7f3190fc834c0b894923f572e1de9ddd309c364a
Jenrick Aran <145414466+jenrickaran@users.noreply.github.com>
2026-08-13 15:48:19 +0800

    Refactor Supabase initialization to use environment variables

--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,16 @@ import 'package:flutter_app/providers/post_provider.dart';
 import 'package:flutter_app/providers/profile_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: '.env');
 
   await Supabase.initialize(
-    url: dotenv.env['SUPABASE_URL']!,
-    publishableKey: dotenv.env['SUPABASE_PUBLISHABLE_KEY']!,
+    url: const String.fromEnvironment('SUPABASE_URL'),
+    publishableKey: const String.fromEnvironment(
+      'SUPABASE_PUBLISHABLE_KEY',
+    ),
   );
 
   runApp(
